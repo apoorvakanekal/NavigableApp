3ec43abf2e80afe36a8f1db3df985fbf52a41e1e
Apoorva Kanekal <apka8010@colorado.edu>
2023-09-14 21:48:55 -0600

    Initial Commit

---
+++ NavigableApp/NavigableAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigableAppApp.swift
+//  NavigableApp
+//
+//  Created by Apoorva Kanekal on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigableAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
